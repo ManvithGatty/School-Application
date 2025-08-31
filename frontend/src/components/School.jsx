864c076f72bc9bb02d35feb947777c384f5ec791
ManvithGatty <manvithgatty87@gmail.com>
2025-08-31 17:38:59 +0530

    Frontend - Display Schools functionality

--- frontend/src/components/School.jsx
+++ frontend/src/components/School.jsx
@@ -1,8 +1,12 @@
 
 
-function School() {
+function School({school}) {
   return (
-    <div>School</div>
+    <div className="school-card">
+      <img src={`http://localhost:5000/images/${school.image}`} alt={school.name} />
+      <h3>{school.name}</h3>
+      <p>{school.address}, {school.city}</p>
+    </div>
   )
 }
 
--- frontend/src/pages/ShowSchools.jsx
+++ frontend/src/pages/ShowSchools.jsx
@@ -1,9 +1,26 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import School from "../components/School.jsx";
 
 function ShowSchools() {
-  return (
-    <div>Show Schools</div>
-  )
+  const [schools, setSchools] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/schools")
+      .then((res) => setSchools(res.data));
+  }, []);
+
+  return(
+    <div>
+      <h2>Schools</h2>
+      <div className="grid">
+        {schools.map(school => (
+          <School key={school.id} school={school} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ShowSchools;
