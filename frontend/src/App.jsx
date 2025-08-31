a62dfffad1faffa462bdbd00a9e1b79234722759
ManvithGatty <manvithgatty87@gmail.com>
2025-08-31 18:24:04 +0530

    Frontend - Added Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddSchools from "./pages/AddSchools.jsx";
 import ShowSchools from "./pages/showSchools.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<ShowSchools/>} />
-        <Route path="/add" element={<AddSchools/>} />
-      </Routes>
+      <div className="container">
+        <Navbar />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<ShowSchools />} />
+            <Route path="/add" element={<AddSchools />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
