188e26e52054246a8484e50ee414fdf628afdb4c
ManvithGatty <manvithgatty87@gmail.com>
2025-09-01 22:53:47 +0530

    Added lazy loading functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AddSchools from "./pages/AddSchools.jsx";
-import ShowSchools from "./pages/ShowSchools.jsx";
+import { lazy,Suspense } from "react";
+import Loading from "./components/Loading.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
+
+const ShowSchools = lazy(() => import("./pages/ShowSchools.jsx"));
+const AddSchools = lazy(() => import("./pages/AddSchools.jsx"));
 
 function App() {
   return (
@@ -10,10 +13,12 @@ function App() {
       <div className="container">
         <Navbar />
         <main className="main">
+          <Suspense fallback={<Loading />}>
           <Routes>
             <Route path="/" element={<ShowSchools />} />
             <Route path="/add" element={<AddSchools />} />
           </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,10 @@
+function Loading() {
+  return (
+    <div className="loading-page">
+      <div className="spinner"></div>
+      <h2>Loading, please wait...</h2>
+    </div>
+  );
+}
+
+export default Loading;
