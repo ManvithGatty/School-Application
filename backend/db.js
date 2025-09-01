8bb69a1168daadf7a857734e8ee2f71d9591c0f6
ManvithGatty <manvithgatty87@gmail.com>
2025-09-01 18:41:00 +0530

    Backend - Changed local db to Aiven Db

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,19 @@
-import mysql from 'mysql2';
+import mysql from "mysql2";
 
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "root",
-    database : "schooldb"
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS, 
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT,
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL DB Connected");
+  if (err) {
+    console.error("MySQL connection error:", err);
+    return;
+  }
+  console.log("Connected to Aiven MySQL");
 });
 
 export default db;
